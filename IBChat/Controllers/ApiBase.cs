20b0a235fc9e8a32aa3630ad729c26ef025e8cef
urauymanov <urauymanov@yandex.ru>
2021-07-05 22:54:11 +0700

    Adding base API controller

---
+++ IBChat/Controllers/ApiBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IBChat.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiBase : ControllerBase
+    {
+        
+    }
+}
