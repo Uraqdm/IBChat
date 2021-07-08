f3bf1b945dcc0637a9bc1940a33a7316443f7622
urauymanov <urauymanov@yandex.ru>
2021-07-08 13:18:19 +0700

    Add application db context via EF

---
+++ IBChat/Domain/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IBChat.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IBChat.Domain.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions options): base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+    }
+}
