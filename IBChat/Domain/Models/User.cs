9c63ab7500bfc1b6a28f1f4d1a394f8758700ea2
urauymanov <urauymanov@yandex.ru>
2021-07-08 13:09:49 +0700

    Add user model

---
+++ IBChat/Domain/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IBChat.Domain.Models
+{
+    public class User
+    {
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public List<Chat> Chats { get; set; }
+    }
+}
