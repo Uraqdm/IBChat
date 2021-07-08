811e23e1484098fee3843c398be8ef6f393f6f19
urauymanov <urauymanov@yandex.ru>
2021-07-08 14:24:54 +0700

    Add messages api controller

---
+++ IBChat/Controllers/MessagesController.cs
@@ -0,0 +1,57 @@
+﻿using IBChat.Domain.Context;
+using IBChat.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IBChat.Controllers
+{
+    public class MessagesController : ApiBase
+    {
+        private readonly AppDbContext _context;
+
+        public MessagesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{Chat}")]
+        public async Task<ActionResult<IEnumerable<Message>>> GetMessages(Chat chat)
+        {
+            return await _context.Messages.Where(m => m.Chat.Guid == chat.Guid).ToListAsync();
+        }
+
+
+        [HttpGet("{Guid}")]
+        public async Task<ActionResult<Message>> GetMessage(Guid guid)
+        {
+            var message =  await _context.Messages.Where(m => m.Guid == guid).FirstOrDefaultAsync();
+
+            if (message == null) 
+                return NotFound();
+
+            return message;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Message>> AddMessage(Message message)
+        {
+            var msg = new Message
+            {
+                DateTime = message.DateTime,
+                Chat = message.Chat,
+                Sender = message.Sender,
+                Text = message.Text
+            };
+
+            _context.Messages.Add(msg);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMessage), new {Guid = msg.Guid }, msg);
+        }
+
+    }
+}
