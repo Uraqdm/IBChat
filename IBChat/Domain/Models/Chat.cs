0bc7cdf6245629e37ff5bea56bfce60864044163
urauymanov <urauymanov@yandex.ru>
2021-07-08 13:12:05 +0700

    Add Chat model

---
+++ IBChat/Domain/Models/Chat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IBChat.Domain.Models
+{
+    public class Chat
+    {
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public List<User> Members { get; set; }
+        public List<Message> Messages { get; set; }
+
+    }
+}
