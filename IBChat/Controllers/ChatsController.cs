d869b6213a16f184dc1e7efc4e6eb9db0aa3b56c
urauymanov <urauymanov@yandex.ru>
2021-07-18 17:21:39 +0700

    Add chats controller

---
+++ IBChat/Controllers/ChatsController.cs
@@ -0,0 +1,81 @@
+﻿using IBChat.Domain.Context;
+using IBChat.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IBChat.Controllers
+{
+    public class ChatsController : ApiBase
+    {
+        private readonly AppDbContext _context;
+
+        public ChatsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Chat>> GetChats()
+        {
+            return await _context.Chats.ToListAsync();
+        }
+
+        [HttpGet("{Guid}")]
+        public async Task<Chat> GetChat(Guid guid)
+        {
+            return await _context.Chats.Where(c => c.Guid == guid).FirstOrDefaultAsync();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateChat(Chat chat)
+        {
+            _context.Chats.Add(chat);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetChat), new { Guid = chat.Guid}, chat);
+        }
+
+        [HttpPut("{Guid}")]
+        public async Task<IActionResult> ChangeChat(Guid guid, Chat chat)
+        {
+            if (guid != chat.Guid) return BadRequest();
+
+            var _chat = await _context.Chats.FindAsync(guid);
+
+            _chat.Name = chat.Name;
+            _chat.Messages = chat.Messages;
+            _chat.Members = chat.Members;
+
+            try
+            {
+               await  _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+            
+        }
+
+        [HttpDelete("{Guid}")]
+        public async Task<IActionResult> DeleteChat(Guid guid)
+        {
+            var chat = await _context.Chats.FindAsync(guid);
+
+            if (chat == null) return NotFound();
+
+            _context.Chats.Remove(chat);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
