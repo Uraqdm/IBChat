5ae15f7a7b08d5c6fd5f701b725448caee27e30c
urauymanov <urauymanov@yandex.ru>
2021-07-20 20:55:47 +0700

    add delegate command

--- IBGChatDesctop/Commands/DelegateCommand.cs
+++ IBGChatDesctop/Commands/DelegateCommand.cs
@@ -1,13 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace IBGChatDesctop.Commands
 {
-    public class DelegateCommand
+    public class DelegateCommand : ICommand
     {
-        
+        #region fields
+        private readonly Action<object> execute;
+        private readonly Func<object, bool> canExecute;
+        #endregion
+
+        #region events
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        #endregion
+
+        #region constructor
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        #endregion
+
+        #region public methods
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute(parameter);
+        }
+        #endregion
     }
 }
