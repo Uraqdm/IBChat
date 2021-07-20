42d68a6f1b8630c2f925780bb634d8078ca9326a
urauymanov <urauymanov@yandex.ru>
2021-07-20 20:36:52 +0700

    add base vm and folders for vm and views

---
+++ IBGChatDesctop/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace IBGChatDesctop.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
