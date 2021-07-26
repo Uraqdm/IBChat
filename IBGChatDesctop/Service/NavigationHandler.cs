fe48db350a8821e3e8849b43d5f6dd2be9f8f43f
urauymanov <urauymanov@yandex.ru>
2021-07-26 21:44:38 +0700

    adding navigation handler for navigation between pages

---
+++ IBGChatDesctop/Service/NavigationHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Windows.Navigation;
+
+namespace IBGChatDesctop.Service
+{
+    public static class NavigationHandler
+    {
+        public static NavigationService NavigationService { get; set; }
+        public static NavigationService MenuNavigationService { get; set; }
+    }
+}
