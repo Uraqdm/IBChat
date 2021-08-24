22276347a4fe46f7f0e7b2dfb08876a099dee7a6
urauymanov <urauymanov@yandex.ru>
2021-08-24 18:09:21 +0700

    Modifying design. Adding converter for messages aligment.

---
+++ IBGChatDesctop/Converters/CurrentUserToAlignConverter.cs
@@ -0,0 +1,22 @@
+﻿using IBGChatDesctop.ViewModels;
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace IBGChatDesctop.Converters
+{
+    public class CurrentUserToAlignConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (Guid)value == MainPageViewModel.CurrentUser.Id ? Dock.Right : Dock.Left;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
